23bbda04f4c2edb102c4897f334ade60f3cb5cc1
MaulRai <muhammadraihanofc@gmail.com>
2025-03-28 20:01:12 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Mendaftar Lowongan
---
+++ src/main/java/id/ac/ui/cs/advprog/mendaftarlowongan/MendaftarLowonganApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.mendaftarlowongan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MendaftarLowonganApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MendaftarLowonganApplication.class, args);
+    }
+
+}
