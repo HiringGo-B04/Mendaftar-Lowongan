53001ea71fd3ba2e66bba9c04879fadd99067a80
MaulRai <muhammadraihanofc@gmail.com>
2025-04-09 06:59:35 +0700

    feat: implement User, Mahasiswa, Dosen

---
+++ src/main/java/id/ac/ui/cs/advprog/mendaftarlowongan/model/Dosen.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.mendaftarlowongan.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Dosen extends User {
+    private String fullName;
+    private String nip;
+}
---
+++ src/main/java/id/ac/ui/cs/advprog/mendaftarlowongan/model/User.java
@@ -0,0 +1,14 @@
+package id.ac.ui.cs.advprog.mendaftarlowongan.model;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter @Setter
+public class User {
+    private UUID id;
+
+    private String email;
+    private String password;
+}
+
