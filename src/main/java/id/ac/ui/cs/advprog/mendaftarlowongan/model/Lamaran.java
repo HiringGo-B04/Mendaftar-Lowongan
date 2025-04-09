d531f11a06e970fccfcdcad5b7ca635d16a37db8
MaulRai <muhammadraihanofc@gmail.com>
2025-04-09 07:04:04 +0700

    feat: implement Lamaran

---
+++ src/main/java/id/ac/ui/cs/advprog/mendaftarlowongan/model/Lamaran.java
@@ -0,0 +1,19 @@
+package id.ac.ui.cs.advprog.mendaftarlowongan.model;
+import id.ac.ui.cs.advprog.mendaftarlowongan.enums.StatusLamaran;
+import lombok.Setter;
+import lombok.Getter;
+import java.util.UUID;
+
+import jakarta.persistence.*;
+
+@Getter @Setter
+public class Lamaran {
+    private UUID id;
+
+    private int sks;
+    private float gpa;
+
+    private StatusLamaran status;
+    private Mahasiswa mahasiswa;
+    private Lowongan lowongan;
+}
