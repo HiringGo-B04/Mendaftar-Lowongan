fcccdd02a0b9fd0cdc3b724d8519095208dca0b8
MaulRai <muhammadraihanofc@gmail.com>
2025-04-30 01:51:04 +0700

    [GREEN] Implement LamaranRepository model.

--- src/main/java/id/ac/ui/cs/advprog/mendaftarlowongan/repository/LamaranRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/mendaftarlowongan/repository/LamaranRepository.java
@@ -1,11 +1,13 @@
 package id.ac.ui.cs.advprog.mendaftarlowongan.repository;
 
 import id.ac.ui.cs.advprog.mendaftarlowongan.model.Lamaran;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public class LamaranRepository {
     private static LamaranRepository instance;
     private List<Lamaran> daftarLamaran;
@@ -22,21 +24,34 @@ public class LamaranRepository {
     }
 
     public List<Lamaran> getLamaran() {
-        return null;
+        return daftarLamaran;
     }
 
     public Lamaran getLamaranById(UUID id) {
-        return null;
+        return daftarLamaran.stream()
+                .filter(lamaran -> lamaran.getId().equals(id))
+                .findFirst()
+                .orElse(null);
     }
 
     public Lamaran createLamaran(Lamaran lamaran) {
-        return null;
+        daftarLamaran.add(lamaran);
+        return lamaran;
     }
 
     public Lamaran updateLamaran(UUID id, Lamaran updatedLamaran) {
-        return null;
+        Lamaran existingLamaran = getLamaranById(id);
+        if (existingLamaran != null) {
+            existingLamaran.setSks(updatedLamaran.getSks());
+            existingLamaran.setIpk(updatedLamaran.getIpk());
+            existingLamaran.setStatus(updatedLamaran.getStatus());
+            existingLamaran.setIdMahasiswa(updatedLamaran.getIdMahasiswa());
+            existingLamaran.setIdLowongan(updatedLamaran.getIdLowongan());
+        }
+        return existingLamaran;
     }
 
     public void deleteLamaran(UUID id) {
+        daftarLamaran.removeIf(lamaran -> lamaran.getId().equals(id));
     }
 }
